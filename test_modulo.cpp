d751127e740d3393f46eb7741a4225956f2fd1a0
Xiang Sun <tsiangsun@gmail.com>
2018-07-10 16:21:41 -0700

    Updated 7/10/18

---
+++ test_modulo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+inline int positive_modulo(int i, int n);
+
+int main (int argc, char *argv[]) {
+    int a = 5;
+    int b = 10;
+    int c = -3;
+    
+    cout << "5 % 10 = " << a%b << endl;
+    cout << "-3 % 10 = " << c%b << endl;
+    cout << "positive_modulo(-3, 10) = " << positive_modulo(c,b) << endl;
+    
+    return (0);
+}
+
+inline int positive_modulo(int i, int n) {
+    return (i % n + n) % n;
+}
